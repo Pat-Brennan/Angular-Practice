5a2640c9fe658800535bdddae776e14b43a092af
Patrick Brennan <patbrennan7@gmail.com>
2023-01-12 15:07:42 -0500

    First angular app looks great

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title: string;
+  numberOne: number;
+  numberTwo: number;
+  togglePokemon: false;
+  imgSrc: string;
+  favoriteAnimal: string;
+
+  constructor() {
+    this.title = "Basket of baby BEAVS";
+    this.numberOne = 666;
+    this.numberTwo = 420;
+    this.togglePokemon = false;
+    this.imgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiisp2e-J2wZHuh-Vntw0yOxa8t929Pv1hpuiUwm8-vw&s"
+    this.favoriteAnimal = "TURTS"
+  }
 }
