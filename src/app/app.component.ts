7a2022328e00fc6bee6967ce38fbc416af9229ce
Patrick Brennan <patbrennan7@gmail.com>
2023-01-12 16:14:41 -0500

    Attempted to cycle through list of pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { Form } from '@angular/forms';
 
+interface Pokemon {
+  id: number,
+  name: string,
+  type: string,
+  isCool: boolean
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +20,24 @@ export class AppComponent {
   togglePokemon: false;
   imgSrc: string;
   favoriteAnimal: string;
+  pokemonName: string;
+
+  pokemons: Pokemon[] = [{
+    id: 1,
+    name: "Charmander",
+    type: "Fire",
+    isCool: false
+  }, {
+    id: 2,
+    name: "Bulbasaur",
+    type: "Grass",
+    isCool: true
+    }, {
+    id: 3,
+    name: "Squirtle",
+    type: "Water",
+    isCool: true
+  }]
 
   constructor() {
     this.title = "Basket of baby BEAVS";
@@ -21,5 +46,10 @@ export class AppComponent {
     this.togglePokemon = false;
     this.imgSrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiisp2e-J2wZHuh-Vntw0yOxa8t929Pv1hpuiUwm8-vw&s"
     this.favoriteAnimal = "TURTS"
+    this.pokemonName = ""
+  }
+
+  handleClick(value: any) {
+    console.log(value)
   }
 }
